0c1bacc6c10d1ec5a04e7bcdf288d802c8a1d9eb
LydeVN <julesvannelli@gmail.com>
2026-07-15 16:36:02 +0200

    yghtjfr

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 // src/config.js
 export const API_CONFIG = {
-  // En local, ton FastAPI tourne sur localhost:8000
-  // Quand tu déploieras, tu mettras ici l'IP publique de ton VPS
-  API_BASE_URL: "http://164.132.111.232:8000",
-  WS_BASE_URL: "ws://164.132.111.232:8000"
+  // Grâce au Reverse Proxy Nginx, on passe directement par ton nom de domaine sécurisé
+  API_BASE_URL: "https://opal.lydecorp.fr",
+  WS_BASE_URL: "wss://opal.lydecorp.fr"
 };
