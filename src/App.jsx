f8d2bfb57d2e854763d677b1b02c05ab573c1828
LydeVN <julesvannelli@gmail.com>
2026-07-15 18:30:15 +0200

    sdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
 import PalworldMap from './components/PalworldMap';
-import PlayersDashboard from './components/PlayersDashboard';
 
 function App() {
   return (
-    <Router>
-      <div className="w-screen h-screen overflow-hidden bg-[#0b0f19]">
-        <Routes>
-          {/* Page d'accueil avec les deux grands boutons futuristes */}
-          <Route path="/" element={<Home />} />
-          
-          {/* Page de la carte interactive (Radar) */}
-          <Route path="/map" element={<PalworldMap />} />
-          
-          {/* Page du registre des joueurs (Dashboard) */}
-          <Route path="/players" element={<PlayersDashboard />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="w-screen h-screen overflow-hidden">
+      <PalworldMap />
+    </div>
   );
 }
 
